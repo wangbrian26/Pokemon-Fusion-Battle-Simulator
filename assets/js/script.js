e2bb3175c9d4c32c01a77876586958fdefd489c6
Brian Wang <wangbrian26@gmail.com>
2022-11-23 11:20:46 -0800

    created fusionPokemon function

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// fetch request for fusion pokemon
+
+function fusionPokemon() {
+  fetch("https://keith.api.stdlib.com/pokefusion@0.2.0/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var poke1 = data.fused.body.toLowerCase();
+      var poke2 = data.fused.head.toLowerCase();
+      var fusionPokemonImg = data.image_url;
+      fetch("https://pokeapi.co/api/v2/pokemon/" + poke1)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          var poke1Hp = data.stats[0].base_stat;
+          var poke1Attack = data.stats[1].base_stat;
+          var poke1Defense = data.stats[2].base_stat;
+          var poke1Speed = data.stats[5].base_stat;
+          poke1Attack = poke1Attack / 2;
+          poke1Hp = poke1Hp + poke1Defense / 2;
+          console.log(poke1Attack);
+          console.log(poke1Hp);
+          console.log(poke1Speed);
+          fetch("https://pokeapi.co/api/v2/pokemon/" + poke2)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data);
+              var poke2Hp = data.stats[0].base_stat;
+              var poke2Attack = data.stats[1].base_stat;
+              var poke2Defense = data.stats[2].base_stat;
+              var poke2Speed = data.stats[5].base_stat;
+              poke2Attack = poke2Attack / 2;
+              poke2Hp = poke2Hp + poke2Defense / 2;
+              console.log(poke2Attack);
+              console.log(poke2Hp);
+              console.log(poke2Speed);
+              var fusionPokemonAttack = (poke1Attack + poke2Attack) / 2;
+              var fusionPokemonHp = (poke1Hp + poke2Hp) / 2;
+              var fusionPokemonSpeed = (poke1Speed + poke2Speed) / 2;
+              console.log(fusionPokemonAttack);
+              console.log(fusionPokemonHp);
+              console.log(fusionPokemonSpeed);
+            });
+        });
+    });
+}
+
+fusionPokemon();
