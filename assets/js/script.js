52d3ae673e800f3b75d1520fa864ad9b9f3ec982
Nicole T <nbtrieu@uci.edu>
2022-11-23 14:39:18 -0800

    testing out js for fakemon random generator

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// Nicole's Landing Page JS
+
+
+let fakemonPaths = [
+    './assets/images/fakemon/1.jpeg',
+    './assets/images/fakemon/2.jpeg', 
+    './assets/images/fakemon/3.jpeg',
+    './assets/images/fakemon/4.jpeg',
+    './assets/images/fakemon/5.jpeg',
+    './assets/images/fakemon/6.jpeg',
+    './assets/images/fakemon/7.jpeg',
+    './assets/images/fakemon/8.jpeg',
+    './assets/images/fakemon/9.jpeg',
+    './assets/images/fakemon/10.jpeg',
+    './assets/images/fakemon/11.jpeg',
+    './assets/images/fakemon/12.jpeg',
+    './assets/images/fakemon/13.jpeg',
+    './assets/images/fakemon/14.jpeg',
+    './assets/images/fakemon/15.jpeg',
+    './assets/images/fakemon/16.jpeg',
+    './assets/images/fakemon/17.jpeg',
+    './assets/images/fakemon/18.jpeg',
+    './assets/images/fakemon/19.png',
+    './assets/images/fakemon/20.png',
+    './assets/images/fakemon/21.png',
+    './assets/images/fakemon/22.png',
+    './assets/images/fakemon/23.png',
+    './assets/images/fakemon/24.png',
+]
+
+document.getElementById('choice1').src = fakemonPaths[0];
+document.getElementById('choice2').src = fakemonPaths[18];
+document.getElementById('choice3').src = fakemonPaths[23];
+
+document.getElementById('choice4').src = fakemonPaths[20];
+document.getElementById('choice5').src = fakemonPaths[9];
+
+document.getElementById('choice1').width = "300";
+document.getElementById('choice1').height = "500";
+
+document.getElementById('choice2').width = "300";
+document.getElementById('choice2').height = "500";
+
+document.getElementById('choice3').width = "300";
+document.getElementById('choice3').height = "500";
+
+document.getElementById('choice4').width = "300";
+document.getElementById('choice4').height = "500";
+
+document.getElementById('choice5').width = "300";
+document.getElementById('choice5').height = "500";
